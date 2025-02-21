5eab3ae44cf1180640665a404c755b0e059522de
dipanshu18 <torawanedipanshu@gmail.com>
2025-02-21 21:40:32 +0530

    added user profile feeds with authenticated, updateOwn permission

--- profiles_api/profiles_api/models.py
+++ profiles_api/profiles_api/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
     BaseUserManager,
 )
+from django.conf import settings
 
 
 class UserProfileManager(BaseUserManager):
@@ -71,3 +72,19 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         Return string representation of our user
         """
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """
+    Feed associated with user created it
+    """
+
+    user_profile = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """
+        Return the model as a string
+        """
+        return self.status_text
--- profiles_api/profiles_api/serializers.py
+++ profiles_api/profiles_api/serializers.py
@@ -39,3 +39,14 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """
+    Serializes a profile feed object
+    """
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ("id", "user_profile", "status_text", "created_at")
+        extra_kwargs = {"user_profile": {"read_only": True}}
