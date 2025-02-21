cdf3f2569e93de530033ab74c89d8cbb9d1cba0f
dipanshu18 <torawanedipanshu@gmail.com>
2025-02-21 18:00:22 +0530

    profiles_api custom user model and migrations

--- profiles_api/config/settings.py
+++ profiles_api/config/settings.py
@@ -121,4 +121,6 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+AUTH_USER_MODEL = "profiles_api.UserProfile"
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
